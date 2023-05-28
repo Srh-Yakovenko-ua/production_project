1f8947fe478ce70c4014cab4fda667bbb557c25d
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-05-28 22:41:29 +0300

    style organization adding a theme

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,26 @@ import React, {Suspense} from 'react';
 import {Link, Route, Routes} from 'react-router-dom';
 import {AboutPageAsync} from './pages/AboutPage/About.async';
 import {MainPageAsync} from './pages/MainPage/Main.async';
+import './styles/index.scss'
+import {useTheme} from './theme/useTheme';
+
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div>
-            <div><Link to="/">main</Link>
-                <Link to="/about">about</Link></div>
-            <Suspense fallback={<span>loading...</span>}><Routes>
-                <Route path="/" element={<MainPageAsync/>}/>
-                <Route path="/about" element={<AboutPageAsync/>}/>
-            </Routes></Suspense>
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
+            <div>
+                <Link to="/">main</Link>
+                <Link to="/about">about</Link>
+            </div>
+            <Suspense fallback={<span>loading...</span>}>
+                <Routes>
+                    <Route path="/" element={<MainPageAsync/>}/>
+                    <Route path="/about" element={<AboutPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import {render} from 'react-dom';
-import './index.scss'
+import './styles/index.scss'
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
+import {ThemeProvider} from './theme/ThemeProvider';
 
 render(
     <div className="app">
-        <BrowserRouter><App/></BrowserRouter>
+        <BrowserRouter>
+            <ThemeProvider>
+                <App/>
+            </ThemeProvider>
+        </BrowserRouter>
     </div>,
     document.getElementById('root')
 )
