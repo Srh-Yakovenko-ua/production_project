f8a1ed9ed64289f7acafc71c0a5e904507289a39
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-06-06 21:54:09 +0300

    add ui-kit button

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,23 @@
+import React, {ButtonHTMLAttributes, FC} from 'react';
+import {cls} from 'shared/lib/classNames/classNames';
+import style from './Button.module.scss'
+
+export enum ThemeButton {
+    CLEAR = 'clear',
+}
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    className?: string
+    theme?: ThemeButton
+}
+
+export const Button: FC<ButtonProps> = ({className, theme, children, ...otherProps}) => {
+    return (
+        <button className={cls(style.Button, {}, [className, style[theme]])}
+                {...otherProps}
+        >
+            {children}
+        </button>
+    );
+};
+
