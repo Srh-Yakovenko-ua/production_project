7d44fa97dc9f6633a0e2749ed06e33508dd2bd7c
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-06-06 21:53:55 +0300

    add theme-switcher

---
+++ src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
@@ -0,0 +1,26 @@
+import React, {FC} from 'react';
+import {cls} from 'shared/lib/classNames/classNames';
+import style from './ThemeSwitcher.module.scss'
+import {Theme, useTheme} from 'app/providers/ThemeProvider';
+import LightIcon from 'shared/assets/icons/theme-light.svg';
+import DarkIcon from 'shared/assets/icons/theme-dark.svg';
+import {Button, ThemeButton} from 'shared/ui/Button/Button';
+
+
+interface ThemeSwitcherProps {
+    className?: string
+}
+
+export const ThemeSwitcher: FC<ThemeSwitcherProps> = ({className}) => {
+
+    const {toggleTheme, theme} = useTheme()
+    return (
+        <Button
+            className={cls(style.ThemeSwitcher, {}, [className])}
+           theme={ThemeButton.CLEAR}
+            onClick={toggleTheme}>
+            {theme === Theme.DARK ? <DarkIcon/> : <LightIcon/>}
+        </Button>
+    );
+};
+
