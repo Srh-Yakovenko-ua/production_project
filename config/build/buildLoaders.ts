544f85dee629a3f405a254c43efdd7d9d4ba2edc
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-06-06 21:54:42 +0300

    build(webpack) : add svg loader and file loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,6 +4,28 @@ import {BuildOptions} from './types/config';
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    }
+
+    const svgLoader = {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
+    }
+
+    //Если не используем ts - нужен babel-loader
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
     const scssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -28,14 +50,11 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         ],
     }
 
-    //Если не используем ts - нужен babel-loader
-    const typescriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    }
+
 
     return [
+        fileLoader,
+        svgLoader,
         typescriptLoader,
         scssLoader,
     ]
