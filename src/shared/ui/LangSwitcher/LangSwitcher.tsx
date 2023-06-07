7094e41db715c449a7180625917467422b533d98
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-06-07 14:30:00 +0300

    add lang-switcher

---
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -0,0 +1,24 @@
+import React, {FC} from 'react';
+import {cls} from 'shared/lib/classNames/classNames';
+import style from './LangSwitcher.module.scss'
+import {useTranslation} from 'react-i18next';
+import {Button, ThemeButton} from 'shared/ui/Button/Button';
+
+interface LangSwitcherProps {
+    className?: string
+}
+
+export const LangSwitcher: FC<LangSwitcherProps> = ({className}) => {
+
+    const {t, i18n} = useTranslation()
+    const toggle = () => {
+        i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru')
+    }
+
+    return (
+        <Button className={cls(style.LangSwitcher, {}, [className])} onClick={toggle} theme={ThemeButton.CLEAR}>
+            {t('Язык')}
+        </Button>
+    );
+};
+
