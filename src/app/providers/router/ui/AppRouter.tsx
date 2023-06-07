3de10fd6850b24d6e82d637010c2d06444be5c03
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-06-07 13:26:01 +0300

    style for app

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -7,7 +7,12 @@ export const AppRouter = () => {
         <Suspense fallback={<span>loading...</span>}>
             <Routes>
                 {Object.values(routerConfig).map(({element, path}) => (
-                    <Route key={path} element={element} path={path}/>
+                    <Route key={path} element={
+                        <div className="page-wrapper">
+                            {element}
+                        </div>
+                    }
+                           path={path}/>
                 ))}
             </Routes>
         </Suspense>
