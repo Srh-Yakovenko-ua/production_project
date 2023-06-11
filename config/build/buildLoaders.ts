0587e57adaad0333662afd7df2185353efed96c9
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2023-06-11 20:14:10 +0300

    webpack : add babel and extract for i18n

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,7 +3,7 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import {BuildOptions} from './types/config';
 
 export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
-
+    // для файлов , картинок и т.п
     const fileLoader = {
         test: /\.(png|jpe?g|gif)$/i,
         use: [
@@ -12,20 +12,41 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
             },
         ],
     }
+    // для js
+    const babelLoader = {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    ['i18next-extract',
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true
+                        }
+                    ],
+                ]
+            }
+        }
+    }
 
+    // для svg как компоненты
     const svgLoader = {
         test: /\.svg$/i,
         issuer: /\.[jt]sx?$/,
         use: ['@svgr/webpack'],
     }
 
-    //Если не используем ts - нужен babel-loader
+    //Если не используем ts - нужен babel-loader для Js
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
     }
 
+    // для стилей имена и сжатие
     const scssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -51,10 +72,10 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     }
 
 
-
     return [
         fileLoader,
         svgLoader,
+        babelLoader,
         typescriptLoader,
         scssLoader,
     ]
--- config/build/buildPlugin.ts
+++ config/build/buildPlugin.ts
@@ -5,17 +5,22 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 export function buildPlugin({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
     return [
+        // для индекс файла (страта апп)
         new HTMLWebpackPlugin({
             template: paths.html
         }),
+        //для процесса сколько прошло
         new webpack.ProgressPlugin(),
+        // для стилей имена
         new MiniCssExtractPlugin({
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css',
         }),
+        // для глобальных перменных
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev)
         }),
+        // hot режим
         new webpack.HotModuleReplacementPlugin(),
     ]
 
